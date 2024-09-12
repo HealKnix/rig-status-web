0bca8680d605e09f7587af240d05f2cdeb6469bf
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-09-12 19:44:18 +0800

    update: Update progressbar

--- client/src/components/ProgressBar/ProgressBar.tsx
+++ client/src/components/ProgressBar/ProgressBar.tsx
@@ -5,19 +5,32 @@ import { ProgressHTMLAttributes } from 'react';
 interface ProgressBarProps extends ProgressHTMLAttributes<HTMLProgressElement> {
   color?: string;
   loader?: boolean;
+  min?: number;
+  max?: number;
+  value: number;
+  defaultValue?: number;
 }
 
 export default function ProgressBar({
   color,
   loader = false,
-  ...props
+  min = 0,
+  max = 100,
+  value,
+  defaultValue = 0,
 }: Readonly<ProgressBarProps>) {
+  const currentMaxValue = max - min;
+  const currentValue = ((value - min) / currentMaxValue) * 100;
+
   return (
-    <progress
-      className={`progress-bar ${loader ? 'in-progress' : ''}`}
-      style={{ '--accentColor': color } as React.CSSProperties}
-      defaultValue={0}
-      {...props}
+    <div
+      className={`progress-bar${loader ? ' in-progress' : ''}`}
+      style={
+        {
+          '--width': `${currentValue ?? defaultValue}%`,
+          '--accentColor': color,
+        } as React.CSSProperties
+      }
     />
   );
 }
